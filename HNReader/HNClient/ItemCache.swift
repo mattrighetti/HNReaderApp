b3b4f38dffbe85f4b3964bd5643a20d7c8797c2b
Mattia Righetti <matt95.righetti@gmail.com>
2021-06-13 22:52:16 +0200

    ItemCache implementation
    + Items can now be cached by their ID is a NSCache
    + Protocol ItemDownloader will be used by each ItemCell that will
      individually handle item download

---
+++ HNReader/HNClient/ItemCache.swift
@@ -0,0 +1,30 @@
+//
+// Created by Mattia Righetti on 13/06/21.
+//
+
+import Foundation
+
+class StructWrapper<T>: NSObject {
+    let value: T
+
+    init(_ _struct: T) {
+        value = _struct
+    }
+}
+
+
+class ItemCache: NSCache<NSString, StructWrapper<Item>> {
+    static let shared = ItemCache()
+
+    func cache(_ item: Item, for key: Int) {
+        let keyString = NSString(format: "%d", key)
+        let itemWrapper = StructWrapper(item)
+        self.setObject(itemWrapper, forKey: keyString)
+    }
+
+    func getItem(for key: Int) -> Item? {
+        let keyString = NSString(format: "%d", key)
+        let itemWrapper = self.object(forKey: keyString)
+        return itemWrapper?.value
+    }
+}
